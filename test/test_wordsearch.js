46f2c0263d887818f0c8d46d313a3de288944ce6
Payam Beigi <beigi.payam@gmail.com>
2022-06-09 04:37:04 +0000

    test file for wordsearch, should be placed inside test directory

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,37 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'M', 'O', 'N', 'K', 'E', 'Y', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MONKEY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'C', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'E', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'S', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'T', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'F', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'I', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'N', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CESTFINI')
+
+    assert.isFalse(result);
+  });
+
 });
