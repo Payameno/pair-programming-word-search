227b561cc1779bf8647155f73b11008299d97de2
Payam Beigi <beigi.payam@gmail.com>
2022-06-09 04:36:08 +0000

    Pair progrraming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
+//Pair programming with @Enired
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  const horizontalJoin = letters.map(ls => ls.join('')); //MAPS THROUGH ARRAYS/CREATE STRING
+  for (s of horizontalJoin) {
+      if (s.includes(word)) return true;
+  };
+
+  let verticalArr = [];
+  for(let i = 0; i < letters[0].length; i++){
+    verticalArr.push([]);
+  };
+
+
+  for(let i=0; i<letters.length; i++){
+    for(let j = 0; j < letters[i].length; j++){
+      verticalArr[j].push(letters[i][j]);
+    };
+  };
+
+  const verticalJoin = verticalArr.map(ls => ls.join(''))
+  for (s of verticalJoin) {
+      if (s.includes(word)) return true;
+  };
+
+  return false;
+
 }
 
 module.exports = wordSearch
